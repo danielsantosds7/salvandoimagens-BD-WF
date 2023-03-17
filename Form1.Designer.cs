2985c2dc4a7ccf65acb2e49ffdd739f37895ec7f
Daniel Santos <danielsantosds7@gmail.com>
2023-03-17 10:21:49 -0300

    salvando imagens no BD e visualizando na propria aplicação

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace SalvandoImagens_BD_SQLServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStringConexaoBD = new System.Windows.Forms.TextBox();
+            this.btnAtivar = new System.Windows.Forms.Button();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.btnRetornarImagemBD = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCodigoImagem = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDescricaoImagem = new System.Windows.Forms.TextBox();
+            this.btnCarregarImagem = new System.Windows.Forms.Button();
+            this.btnSalvarImagemBD = new System.Windows.Forms.Button();
+            this.gdvImagens = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gdvImagens)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "String de Conexão com o SQL Server";
+            // 
+            // txtStringConexaoBD
+            // 
+            this.txtStringConexaoBD.Location = new System.Drawing.Point(36, 69);
+            this.txtStringConexaoBD.Multiline = true;
+            this.txtStringConexaoBD.Name = "txtStringConexaoBD";
+            this.txtStringConexaoBD.Size = new System.Drawing.Size(611, 82);
+            this.txtStringConexaoBD.TabIndex = 1;
+            // 
+            // btnAtivar
+            // 
+            this.btnAtivar.Location = new System.Drawing.Point(653, 69);
+            this.btnAtivar.Name = "btnAtivar";
+            this.btnAtivar.Size = new System.Drawing.Size(113, 82);
+            this.btnAtivar.TabIndex = 2;
+            this.btnAtivar.Text = "Ativar";
+            this.btnAtivar.UseVisualStyleBackColor = true;
+            this.btnAtivar.Click += new System.EventHandler(this.btnAtivar_Click);
+            // 
+            // picImagem
+            // 
+            this.picImagem.Location = new System.Drawing.Point(36, 177);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(382, 410);
+            this.picImagem.TabIndex = 3;
+            this.picImagem.TabStop = false;
+            // 
+            // btnRetornarImagemBD
+            // 
+            this.btnRetornarImagemBD.Enabled = false;
+            this.btnRetornarImagemBD.Location = new System.Drawing.Point(425, 177);
+            this.btnRetornarImagemBD.Name = "btnRetornarImagemBD";
+            this.btnRetornarImagemBD.Size = new System.Drawing.Size(341, 60);
+            this.btnRetornarImagemBD.TabIndex = 4;
+            this.btnRetornarImagemBD.Text = "Obter Imagem do Banco de Dados";
+            this.btnRetornarImagemBD.UseVisualStyleBackColor = true;
+            this.btnRetornarImagemBD.Click += new System.EventHandler(this.btnRetornarImagemBD_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(424, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Código da Imagem";
+            // 
+            // txtCodigoImagem
+            // 
+            this.txtCodigoImagem.Location = new System.Drawing.Point(425, 302);
+            this.txtCodigoImagem.Name = "txtCodigoImagem";
+            this.txtCodigoImagem.Size = new System.Drawing.Size(341, 26);
+            this.txtCodigoImagem.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(424, 352);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Descrição da Imagem";
+            // 
+            // txtDescricaoImagem
+            // 
+            this.txtDescricaoImagem.Location = new System.Drawing.Point(425, 385);
+            this.txtDescricaoImagem.Multiline = true;
+            this.txtDescricaoImagem.Name = "txtDescricaoImagem";
+            this.txtDescricaoImagem.Size = new System.Drawing.Size(341, 202);
+            this.txtDescricaoImagem.TabIndex = 8;
+            // 
+            // btnCarregarImagem
+            // 
+            this.btnCarregarImagem.Enabled = false;
+            this.btnCarregarImagem.Location = new System.Drawing.Point(36, 593);
+            this.btnCarregarImagem.Name = "btnCarregarImagem";
+            this.btnCarregarImagem.Size = new System.Drawing.Size(382, 60);
+            this.btnCarregarImagem.TabIndex = 9;
+            this.btnCarregarImagem.Text = "Carregar Imagem";
+            this.btnCarregarImagem.UseVisualStyleBackColor = true;
+            this.btnCarregarImagem.Click += new System.EventHandler(this.btnCarregarImagem_Click);
+            // 
+            // btnSalvarImagemBD
+            // 
+            this.btnSalvarImagemBD.Location = new System.Drawing.Point(428, 593);
+            this.btnSalvarImagemBD.Name = "btnSalvarImagemBD";
+            this.btnSalvarImagemBD.Size = new System.Drawing.Size(338, 60);
+            this.btnSalvarImagemBD.TabIndex = 10;
+            this.btnSalvarImagemBD.Text = "Salvar Imagem no Banco de Dados";
+            this.btnSalvarImagemBD.UseVisualStyleBackColor = true;
+            this.btnSalvarImagemBD.Click += new System.EventHandler(this.btnSalvarImagemBD_Click);
+            // 
+            // gdvImagens
+            // 
+            this.gdvImagens.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gdvImagens.Location = new System.Drawing.Point(36, 681);
+            this.gdvImagens.Name = "gdvImagens";
+            this.gdvImagens.RowHeadersWidth = 62;
+            this.gdvImagens.RowTemplate.Height = 28;
+            this.gdvImagens.Size = new System.Drawing.Size(730, 276);
+            this.gdvImagens.TabIndex = 11;
+            this.gdvImagens.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gdvImagens_CellContentClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 980);
+            this.Controls.Add(this.gdvImagens);
+            this.Controls.Add(this.btnSalvarImagemBD);
+            this.Controls.Add(this.btnCarregarImagem);
+            this.Controls.Add(this.txtDescricaoImagem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCodigoImagem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRetornarImagemBD);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.btnAtivar);
+            this.Controls.Add(this.txtStringConexaoBD);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gdvImagens)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStringConexaoBD;
+        private System.Windows.Forms.Button btnAtivar;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.Button btnRetornarImagemBD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodigoImagem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescricaoImagem;
+        private System.Windows.Forms.Button btnCarregarImagem;
+        private System.Windows.Forms.Button btnSalvarImagemBD;
+        private System.Windows.Forms.DataGridView gdvImagens;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace SalvandoImagens_BD_SQLServer
+{
+    public partial class Form1 : Form
+    {
+        private long tamanhoArquivoImagem = 0;
+        private byte[] vetorImagens;
+        private SqlConnection conexaoSQLServer;
+        SqlCommand sqlcmd = default(SqlCommand);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Recuperando imagens do banco de dados
+        private void btnAtivar_Click(object sender, EventArgs e)
+        {
+            //cria uma instância do objeto SqlConnection
+            conexaoSQLServer = new SqlConnection(txtStringConexaoBD.Text);
+            //obtém os dados da tabela imagens
+            getImagensSQLServer(conexaoSQLServer);
+            //ativa os botões de comando
+            btnCarregarImagem.Enabled = true;
+            btnRetornarImagemBD.Enabled = true;
+            btnSalvarImagemBD.Enabled = true;
+            btnAtivar.Enabled = true;
+        }
+        void getImagensSQLServer(SqlConnection conexaoSQLServer)
+        {
+            try
+            {
+                //Inicializa o SQL Adapter
+                SqlDataAdapter ADAP = new SqlDataAdapter("Select id, descricao, imagem from Cadastro", conexaoSQLServer);
+
+                //Inicializa o DataSet
+                DataSet DS = new DataSet();
+
+                //Preenche o DataSet com a tabela de Imagens
+                ADAP.Fill(DS, "Cadastro");
+
+                //Preenche o DataGridView com o DataSet
+                gdvImagens.DataSource = DS.Tables["Cadastro"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void gdvImagens_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Obtém o código da imagem e exibe no controle textbox
+            txtCodigoImagem.Text = (gdvImagens.Rows[e.RowIndex].Cells["id"].Value).ToString();
+            txtDescricaoImagem.Text = "";
+        }
+        
+        private void btnCarregarImagem_Click(object sender, EventArgs e)
+        {
+            CarregaImagem();
+            txtCodigoImagem.Text = "";
+            txtDescricaoImagem.Text = "";
+        }
+        protected void CarregaImagem()
+        {
+            try
+            {
+                this.openFileDialog1.ShowDialog(this);
+                string strFn = this.openFileDialog1.FileName;
+
+                if (string.IsNullOrEmpty(strFn))
+                {
+                    return;
+                }
+                else
+                {
+                    this.picImagem.Image = Image.FromFile(strFn);
+                    FileInfo arqImagem = new FileInfo(strFn);
+                    tamanhoArquivoImagem = arqImagem.Length;
+                    FileStream fs = new FileStream(strFn, FileMode.Open, FileAccess.Read, FileShare.Read);
+                    vetorImagens = new byte[Convert.ToInt32(this.tamanhoArquivoImagem)];
+                    int iBytesRead = fs.Read(vetorImagens, 0, Convert.ToInt32(this.tamanhoArquivoImagem));
+                    fs.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //Salvando uma imagem no SQL Server
+        private void btnSalvarImagemBD_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtDescricaoImagem.Text))
+            {
+                MessageBox.Show("Informe a descrição da imagem", "Código da Imagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            try
+            {
+                this.conexaoSQLServer.Open();
+                this.sqlcmd = new SqlCommand();
+                sqlcmd.Connection = conexaoSQLServer;
+                if (sqlcmd.Parameters.Count == 0)
+                {
+                    this.sqlcmd.CommandText = "INSERT INTO Cadastro(descricao,imagem) values(@descricao,@imagem)";
+                    this.sqlcmd.Parameters.Add("@descricao", System.Data.SqlDbType.VarChar, 50);
+                    this.sqlcmd.Parameters.Add("@imagem", System.Data.SqlDbType.Image);
+                }
+                this.sqlcmd.Parameters["@descricao"].Value = this.txtDescricaoImagem.Text;
+                this.sqlcmd.Parameters["@imagem"].Value = this.vetorImagens;
+
+                int iresultado = this.sqlcmd.ExecuteNonQuery();
+
+                if (iresultado <= 0)
+                    MessageBox.Show("Falha ao incluir imagem no banco de dados.");
+
+                getImagensSQLServer(conexaoSQLServer);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                this.conexaoSQLServer.Close();
+            }
+        }
+        //Recuperando UMA imagem do SQL Server
+        private void btnRetornarImagemBD_Click(object sender, EventArgs e)
+        {
+            if (txtCodigoImagem.Text == string.Empty)
+            {
+                MessageBox.Show("Informe o código da imagem no Banco de dados", "Código da Imagem",
+ MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            try
+            {
+                SqlCommand cmdSelect = new SqlCommand("select imagem from Cadastro where id=@ID", this.conexaoSQLServer);
+                cmdSelect.Parameters.Add("@ID", SqlDbType.Int, 4);
+                cmdSelect.Parameters["@ID"].Value = this.txtCodigoImagem.Text;
+
+                this.conexaoSQLServer.Open();
+                byte[] vetorImagem = (byte[])cmdSelect.ExecuteScalar();
+                string strNomeArquivo = Convert.ToString(DateTime.Now.ToFileTime());
+                FileStream fs = new FileStream(strNomeArquivo, FileMode.CreateNew, FileAccess.Write);
+                fs.Write(vetorImagem, 0, vetorImagem.Length);
+                fs.Flush();
+                fs.Close();
+
+                picImagem.Image = Image.FromFile(strNomeArquivo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                this.conexaoSQLServer.Close();
+            }
+        }
+    }
+
+
+
+
+}
